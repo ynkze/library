513f32c2f1e329ad2f33ee0dfdf2af774f273c36
ynkze <teyyiheng@gmail.com>
2021-07-27 22:21:44 -0700

    Change object constructor to class

--- js/script.js
+++ js/script.js
@@ -36,12 +36,14 @@ for(var i =0; i < localStorage.length; i++){
     displayBook(storedBook);
 }
 
-function Book(title, author, pages, read, id){
+class Book{
+    constructor(title, author, pages, read, id){
     this.title=title;
     this.author=author;
     this.pages=pages;
     this.read=read;
     this.id = id;
+    }
 }
 
 function addBookToLibrary(){
